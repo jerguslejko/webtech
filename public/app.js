d35c4ae45da514877f70791310d3b169cacc34dc
Rafael <rd17647@bristol.ac.uk>
2020-04-11 19:58:39 +0100

    basic nav bar

--- public/app.js
+++ public/app.js
@@ -62,3 +62,12 @@ $( ".portlet-toggle" ).click(function() {
     icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
     icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
