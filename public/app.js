09068a221c3ef46ef499f7a1124d6910b98b71f7
Rafael <rd17647@bristol.ac.uk>
2020-04-12 15:27:20 +0100

    basic popups for forms

--- public/app.js
+++ public/app.js
@@ -1,11 +1,10 @@
 window.addEventListener('load', function() {
     //
 });
-
+////////////////////////////////////////////////////////////////////////////////////////
+// BOARDS
 $( ".column" ).sortable({
     connectWith: ".column",
-    handle: ".portlet-header",
-    cancel: ".portlet-toggle",
     start: function (event, ui) {
         ui.item.addClass('tilt');
         tilt_direction(ui.item);
@@ -16,7 +15,6 @@ $( ".column" ).sortable({
         ui.item.removeData("move_handler");
     }
 });
-
 $( ".row" ).sortable({
     connectWith: ".row",
     start: function (event, ui) {
@@ -29,7 +27,6 @@ $( ".row" ).sortable({
         ui.item.removeData("move_handler");
     }
 });
-
 function tilt_direction(item) {
     var left_pos = item.position().left,
         move_handler = function (e) {
@@ -45,24 +42,14 @@ function tilt_direction(item) {
     $("html").bind("mousemove", move_handler);
     item.data("move_handler", move_handler);
 }  
-
 $( ".portlet" )
     .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-    .find( ".portlet-header" )
-    .addClass( "ui-widget-header ui-corner-all" )
-    .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
-
 $( ".column-portlet" )
     .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-    // .find( ".colportlet-header" )
-    // .addClass( "ui-widget-header ui-corner-all" )
-
-$( ".portlet-toggle" ).click(function() {
-    var icon = $( this );
-    icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-    icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
-});
-
+////////////////////////////////////////////////////////////////////////////////////////
+// NAVBAR
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -71,3 +58,64 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////
+// NEW COLUMN POP UP
+// pop up itself
+var newcolmodal = document.getElementById("newcolmodal");
+//button to open and close pop up
+var close_newcolmodal = document.getElementsByClassName("close-newcolmodal")[0];
+close_newcolmodal.onclick = function() {
+    newcolmodal.style.display = "none";
+}
+// buttons to open pop up
+$(".open-newcolmodal").on('click', function(event){
+        newcolmodal.style.display = "block";
+})
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+    if (event.target == newcolmodal) {
+        newcolmodal.style.display = "none";
+    }
+    else if (event.target == newtaskmodal) {
+        newtaskmodal.style.display = "none";
+    }
+
+    else if (event.target == edittaskmodal) {
+        edittaskmodal.style.display = "none";
+    }
+  }
+////////////////////////////////////////////////////////////////////////////////////////
+// NEW TASK POP UP
+// pop up itself
+var newtaskmodal = document.getElementById("newtaskmodal");
+//button to open and close pop up
+var close_newtaskmodal = document.getElementsByClassName("close-newtaskmodal")[0];
+close_newtaskmodal.onclick = function() {
+    newtaskmodal.style.display = "none";
+}
+// buttons to open pop up
+$(".open-newtaskmodal").on('click', function(event){
+    newtaskmodal.style.display = "block";
+})
+////////////////////////////////////////////////////////////////////////////////////////
+//  EDIT TASK POP UP
+// pop up itself
+var edittaskmodal = document.getElementById("edittaskmodal");
+//button to open and close pop up
+var close_edittaskmodal = document.getElementsByClassName("close-edittaskmodal")[0];
+close_edittaskmodal.onclick = function() {
+    edittaskmodal.style.display = "none";
+}
+// buttons to open pop up
+$(".open-edittaskmodal").on('click', function(event){
+    edittaskmodal.style.display = "block";
+})
