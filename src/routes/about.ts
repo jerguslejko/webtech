d881eafe995272c86e9aaba8c782d7955183a2a4
jerguslejko <jergus.lejko@gmail.com>
2020-05-25 17:40:43 +0200

    remove restriction

--- src/routes/about.ts
+++ src/routes/about.ts
@@ -1,10 +1,9 @@
 import * as express from 'express';
-import { restricted } from '../auth';
 
 export default function (app: express.Express): void {
-    app.get('/about', restricted, async (req, res) => {
+    app.get('/about', async (req, res) => {
         res.render('about', {
-            boards: await req.signedIn.boards(),
+            boards: req.signedIn ? await req.signedIn.boards() : [],
         });
     });
 }
