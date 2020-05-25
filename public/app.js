aab3b8b3ae41416e6544cfe95c9b731be325878c
Rafael <rd17647@bristol.ac.uk>
2020-05-25 15:01:43 +0100

    stuff

--- public/app.js
+++ public/app.js
@@ -43,6 +43,22 @@ $('.row').sortable({
         ui.item.removeData('move_handler');
     },
 });
+function isDark( color ) {
+    var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
+    return parseFloat(match[1])
+         + parseFloat(match[2])
+         + parseFloat(match[3])
+           < 3 * 256 / 2; // r+g+b should be less than half of max (3 * 256)
+}
+function disable_board(){
+    $(".row").sortable( "disable" );
+    $(".column").sortable( "disable" );
+}
+function enable_board(){
+
+    $(".row").sortable( "enable" );
+    $(".column").sortable( "enable" );
+}
 function tilt_direction(item) {
     var left_pos = item.position().left,
         move_handler = function (e) {
@@ -82,9 +98,11 @@ window.onscroll = function () {
 // POP UP
 
 $('.modal-wrapper .open-modal').on('click', function () {
+    disable_board();
     $('.modal', $(this).parent('.modal-wrapper')).show();
 });
 $('.modal-wrapper .close-modal').on('click', function () {
+    enable_board();
     $(this).closest('.modal').hide();
 });
 
